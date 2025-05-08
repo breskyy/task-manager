f01e556c749ca66dfe9a3fc7367e25707cb3b003
breskyy <fbrzeski02@gmail.com>
2025-05-08 23:18:28 +0200

    Initial commit - setup Spring Boot project

---
+++ src/main/java/com/github/breskyy/taskmanager/TaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.breskyy.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+}
